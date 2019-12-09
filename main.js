1cb8c87ddf37f4851346b577cc17b84c9316be83
szkapec <szkapec999@o2.pl>
2019-12-10 00:42:51 +0100

    add all project index and style

--- main.js
+++ main.js
@@ -11,20 +11,20 @@ const najechanieMysza = () => {
     odpowiedz.appendChild(gmail)
     console.log(gmail)
 }
-const opuszczenie = () => {
-    gmail.innerHTML = "";
-}
-element.addEventListener('mouseover', najechanieMysza)
-element.addEventListener('mouseout' , opuszczenie)
+// const opuszczenie = () => {
+//     gmail.innerHTML = "";
+// }
+// element.addEventListener('mouseover', najechanieMysza)
+// element.addEventListener('mouseout' , opuszczenie)
 
 
-const najechanieTel = () => {
-    gmail.innerText = "661 360 889"
-    odpowiedz.appendChild(gmail)
-    console.log(gmail)
-}
-const opuszczenieTel = () => {
-    gmail.innerHTML = "";
-}
-telefon.addEventListener('mouseover', najechanieTel) 
-telefon.addEventListener('mouseout', opuszczenieTel) 
+// const najechanieTel = () => {
+//     gmail.innerText = "661 360 889"
+//     odpowiedz.appendChild(gmail)
+//     console.log(gmail)
+// }
+// const opuszczenieTel = () => {
+//     gmail.innerHTML = "";
+// }
+// telefon.addEventListener('mouseover', najechanieTel) 
+// telefon.addEventListener('mouseout', opuszczenieTel) 
