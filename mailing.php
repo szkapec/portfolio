e470b7af940b63c13422d404a1412db37a401bea
szkapec <szkapec999@o2.pl>
2019-12-30 16:58:35 +0100

    add

--- mailing.php
+++ mailing.php
@@ -2,14 +2,13 @@
 $naglowek="Wiadomosc z portfolio";
 
 $adres = $_POST['email'];
-$tytul= "To nowa wiadomość"
+$tytul= "To nowa wiadomosc"
 $tekst = "<html> <head></head>
 <body>
 Witam serdecznie na mojej skrzynce xD
-
 </body>
 </html>";
 
-mail($adres,$tytul,$tekst,$naglowek); 
-echo "Wiadomosc wysłana";
+mail($adres,$tytul,$tekst); 
+echo "Wiadomosc wyslana";
 ?>
